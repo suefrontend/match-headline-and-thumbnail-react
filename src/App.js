922082a20f77b49d7c01f8e2adf251ca50882d90
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-09-08 21:36:12 -0700

    feat: Make API request inside useEffect function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useState, useEffect } from 'react';
 import Headline from './components/Headline';
 import './App.css';
 import ThumbnailList from './components/ThumbnailList';
 
 function App() {
+	const [articles, setArticles] = useState([]);
+
+	useEffect(() => {
+		const fetchArticles = async () => {
+			const data = await fetch(
+				'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Frss.cbc.ca%2Flineup%2Ftopstories.xml'
+			);
+			const response = await data.json();
+			setArticles(response.items);
+		};
+		fetchArticles();
+	}, []);
+
 	return (
 		<div className='App'>
 			<div>
