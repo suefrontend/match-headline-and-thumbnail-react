3ca161e5230c1b5d9e4e72f258d0a11e2c614ccb
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-07-03 00:17:29 -0700

    Add video to Home

--- app/page.js
+++ app/page.js
@@ -1,15 +1,37 @@
 "use client";
 
-import Image from "next/image";
+import React, { useRef, useEffect } from "react";
 import Headline from "@components/Headline";
 import ThumbnailList from "@components/ThumbnailList";
 
 function Home() {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      videoRef.current.play();
+    }, 5000);
+  }, []);
+
   return (
-    <main className="">
-      <Headline />
-      <ThumbnailList />
-    </main>
+    <>
+      <div className="video-wrapper">
+        <video
+          ref={videoRef}
+          src="images/background.mp4"
+          width="250"
+          loop
+          muted
+          className="video"
+        ></video>
+      </div>
+      <main className="wrapper">
+        <div className="content">
+          <Headline />
+          <ThumbnailList />
+        </div>
+      </main>
+    </>
   );
 }
 
