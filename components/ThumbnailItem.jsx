a7f34b7be19076594b414bc0a01300e908d7d675
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-07-03 22:54:14 -0700

    Pass hard coded data and display 4 thumbnails

--- components/ThumbnailItem.jsx
+++ components/ThumbnailItem.jsx
@@ -1,7 +1,32 @@
 "use client";
+import Image from "next/image";
 
-function ThumbnailItem() {
-  return <div>ThumbnailItem</div>;
+function ThumbnailItem(props) {
+  const tempElement = document.createElement("div");
+  tempElement.innerHTML = props.image;
+
+  const imgTag = tempElement.getElementsByTagName("img")[0];
+
+  const src = imgTag.getAttribute("src");
+  const alt = imgTag.getAttribute("alt");
+  const width = imgTag.getAttribute("width");
+  const title = imgTag.getAttribute("title");
+  const height = imgTag.getAttribute("height");
+
+  const pTag = tempElement.getElementsByTagName("p")[0];
+  const pContent = pTag.innerHTML.trim();
+
+  return (
+    <>
+      <li>
+        <figure className="thumbnail__item__img">
+          <Image src={src} alt={alt} width={width} height={height} />
+        </figure>
+        <span className="thumbnail__item__text"></span>
+      </li>
+      {/* <p>p content: {pContent}</p> */}
+    </>
+  );
 }
 
 export default ThumbnailItem;
