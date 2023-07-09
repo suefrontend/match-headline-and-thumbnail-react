ee725797f35ae037e60a4cd6bff8dcaecac69cd2
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-07-09 09:46:13 -0700

    Display data fetched from API

---
+++ components/Video.jsx
@@ -0,0 +1,31 @@
+"use client";
+import { useEffect, useRef } from "react";
+
+function Video(props) {
+  const videoRef = useRef();
+
+  console.log("props", props);
+
+  useEffect(() => {
+    setTimeout(() => {
+      videoRef.current.play();
+    }, 5000);
+  }, []);
+
+  // console.log("video");
+
+  return (
+    <div className="video-wrapper relative">
+      <video
+        ref={videoRef}
+        src="images/background.mp4"
+        width="250"
+        loop
+        muted
+        className="object-cover h-screen w-full"
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
