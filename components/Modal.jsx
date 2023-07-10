016d08089bd7816253b1651710f8cffc3fa75bc0
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-07-10 00:23:49 -0700

    Add overlay on background when modal is opened

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -26,29 +26,36 @@ function Modal(props) {
   const pContent = pTag.innerHTML.trim();
 
   const modalContent = (
-    <div className="detail fixed bg-white">
-      <div className="detail__img">
-        <Image src={src} alt={alt} width={width} height={height} />
-        <AiOutlineCloseCircle
-          className="w-8 h-8 absolute top-4 right-3 cursor-pointer text-white"
-          onClick={handleCloseClick}
-        />
-      </div>
-      <div className="detail__content p-5">
-        <span className="text-grey text-sm">{item.pubDate.split(" ")[0]}</span>
-        <h3 className="mb-3 leading-snug  text-xl font-medium">{item.title}</h3>
-        {pContent}
-        <div className="mt-5">
-          <a
-            className="btn btn-link flex items-center justify-center text-center py-2 transition duration-300 text-btnpink"
-            href={item.link}
-            target="_blank"
-          >
-            Read on CBC website
-          </a>
+    <>
+      <div className="detail fixed bg-white">
+        <div className="detail__img">
+          <Image src={src} alt={alt} width={width} height={height} />
+          <AiOutlineCloseCircle
+            className="w-8 h-8 absolute top-4 right-3 cursor-pointer text-white"
+            onClick={handleCloseClick}
+          />
+        </div>
+        <div className="detail__content p-5">
+          <span className="text-grey text-sm">
+            {item.pubDate.split(" ")[0]}
+          </span>
+          <h3 className="mb-3 leading-snug  text-xl font-medium">
+            {item.title}
+          </h3>
+          {pContent}
+          <div className="mt-5">
+            <a
+              className="btn btn-link flex items-center justify-center text-center py-2 transition duration-300 text-btnpink"
+              href={item.link}
+              target="_blank"
+            >
+              Read on CBC website
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+      <div class="overlay"></div>
+    </>
   );
 
   return createPortal(modalContent, document.body);
